5c7308a6b754b859c7112e34e7f827cb4b534dee
Noé <noe.louvel@viacesi.fr>
2022-09-21 12:36:45 +0200

    Feat(CI/CD) : Test Ci/CD Change APP.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          La CI/CD marche a chaque commit !
         </p>
         <a
           className="App-link"
